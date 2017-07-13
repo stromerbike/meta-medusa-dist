ab115f967391be9a42dcdcdc48a51fd5302ef5ff
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2017-07-13 12:07:14 +0200

    Set FORCE_OTG1_ID pin.

--- recipes-core/systemd/systemd-units/usb.sh
+++ recipes-core/systemd/systemd-units/usb.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 NAME=power
-DESC="Initialization of USB to SDP mode"
+DESC="Initialization of USB to OTG SDP mode"
 
 case $1 in
 start)
+    # FORCE_OTG1_ID
+    echo "23" > /sys/class/gpio/export
+    echo "out" > /sys/class/gpio/gpio23/direction
+    echo "1" > /sys/class/gpio/gpio23/value
+
     # CTL2
     echo "506" > /sys/class/gpio/export
     echo "out" > /sys/class/gpio/gpio506/direction
@@ -12,6 +17,7 @@ start)
 ;;
 
 stop)
+    echo "0" > /sys/class/gpio/gpio23/value
     echo "0" > /sys/class/gpio/gpio506/value
 ;;
 
