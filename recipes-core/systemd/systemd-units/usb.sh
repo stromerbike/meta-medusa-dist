9a7b14388d7e4b5f9a3c7377f8d92468d8eaeffc
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2017-07-13 13:04:16 +0200

    Set 5V_ON (just to be safe even though letting it float would work too).

--- recipes-core/systemd/systemd-units/usb.sh
+++ recipes-core/systemd/systemd-units/usb.sh
@@ -10,6 +10,11 @@ start)
     echo "out" > /sys/class/gpio/gpio23/direction
     echo "1" > /sys/class/gpio/gpio23/value
 
+    # 5V_ON
+    echo "496" > /sys/class/gpio/export
+    echo "out" > /sys/class/gpio/gpio496/direction
+    echo "1" > /sys/class/gpio/gpio496/value
+
     # CTL2
     echo "506" > /sys/class/gpio/export
     echo "out" > /sys/class/gpio/gpio506/direction
@@ -17,8 +22,9 @@ start)
 ;;
 
 stop)
-    echo "0" > /sys/class/gpio/gpio23/value
     echo "0" > /sys/class/gpio/gpio506/value
+    echo "0" > /sys/class/gpio/gpio496/value
+    echo "0" > /sys/class/gpio/gpio23/value
 ;;
 
 *)
