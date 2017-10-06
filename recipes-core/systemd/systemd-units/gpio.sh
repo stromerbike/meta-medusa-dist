aff08d11e3da69e6d573629f3b87eacd8ac5b1c3
oliver.buntschu <oliver.buntschu@stromerbike.com>
2017-10-06 16:48:44 +0200

    blacklisted gpio-expander driver gpio-pca953x and added interrupt support

--- recipes-core/systemd/systemd-units/gpio.sh
+++ recipes-core/systemd/systemd-units/gpio.sh
@@ -5,6 +5,23 @@ DESC="Initialization of internal and external (if present) gpio expander"
 
 case $1 in
 start)
+    echo 18 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio18/direction
+
+    echo 19 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio19/direction
+
+    echo 51 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio51/direction
+    echo 1 > /sys/class/gpio/gpio51/value
+
+    echo 114 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio114/direction
+    echo 1 > /sys/class/gpio/gpio114/value
+
+    i2cset -y 2 0x20 0x06 0xfa
+    i2cset -y 2 0x20 0x02 0xff
+
     modprobe gpio_pca953x
 ;;
 
