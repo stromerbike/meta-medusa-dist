f58c0a386163d388d59dce3bb3ffe8e0b6d66b2b
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2017-12-19 09:02:50 +0100

    Removed workaround for V2 and V4 hardware.

--- recipes-core/systemd/systemd-units/gpio.sh
+++ recipes-core/systemd/systemd-units/gpio.sh
@@ -20,25 +20,20 @@ start)
     echo out > /sys/class/gpio/gpio114/direction
     echo 1 > /sys/class/gpio/gpio114/value
 
-    # turn on the supply voltages 5V & 5V_Out
-    i2cset -y 2 0x20 0x06 0xfa
+    # turn on the 5V_OUT supply voltage
+    i2cset -y 2 0x20 0x06 0xfb
     i2cset -y 2 0x20 0x02 0xff
 
     # load gpio expander driver
     modprobe gpio_pca953x
-    
-    # compatibility for V2 & V4, delete for release-stage
-    echo 498 > /sys/class/gpio/export
-    echo out > /sys/class/gpio/gpio498/direction
-    echo 1 > /sys/class/gpio/gpio498/value
 ;;
 
 stop)
     # unload gpio expander driver
     modprobe -r gpio_pca953x
 
-    # turn off the supply voltages 5V & 5V_Out
-    i2cset -y 2 0x20 0x02 0xfa
+    # turn off the 5V_OUT supply voltage
+    i2cset -y 2 0x20 0x02 0xfb
 ;;
 
 *)
