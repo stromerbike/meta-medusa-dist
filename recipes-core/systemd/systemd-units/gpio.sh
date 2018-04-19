d652a284123e96d64c4f849898f29171e47eeb35
Oliver Buntschu <oliver.buntschu@stromerbike.com>
2018-04-19 09:14:05 +0200

    removed workarounds for the external gpio expander and adapted for new solution in bsp

--- recipes-core/systemd/systemd-units/gpio.sh
+++ recipes-core/systemd/systemd-units/gpio.sh
@@ -21,19 +21,20 @@ start)
     echo 1 > /sys/class/gpio/gpio114/value
 
     # turn on the 5V_OUT supply voltage
-    i2cset -y 2 0x20 0x06 0xfb
-    i2cset -y 2 0x20 0x02 0xff
+    echo 498 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio498/direction
+    echo 1 > /sys/class/gpio/gpio498/value
 
-    # load gpio expander driver
-    modprobe gpio_pca953x
+    # load external gpio expander driver
+    modprobe gpio_pca953x_external
 ;;
 
 stop)
-    # unload gpio expander driver
-    modprobe -r gpio_pca953x
+    # unload external gpio expander driver
+    modprobe -r gpio_pca953x_external
 
     # turn off the 5V_OUT supply voltage
-    i2cset -y 2 0x20 0x02 0xfb
+    echo 0 > /sys/class/gpio/gpio498/value
 ;;
 
 *)
--- recipes-kernel/linux/linux-mainline_%.bbappend
+++ recipes-kernel/linux/linux-mainline_%.bbappend
@@ -6,12 +6,8 @@ SRC_URI += " \
             file://filesystems.cfg \
             file://i2c-devices.cfg \
             file://jtag-pm.patch \
-            file://pca953x_irq.patch \
             file://ppp-network.cfg \
             file://usb-devices.cfg \
             file://virtual-devices.cfg \
             file://wifi-devices.cfg \
 "
-
-KERNEL_MODULE_PROBECONF += " gpio-pca953x"
-module_conf_gpio-pca953x = "blacklist gpio-pca953x"
