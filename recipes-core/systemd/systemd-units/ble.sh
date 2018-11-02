b6c25874dcd7a7980db727c6b0a0cdf07211dcca
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2018-11-02 09:45:06 +0100

    Added detection of TI CC2564B and CC2564C.

--- recipes-core/systemd/systemd-units/ble.sh
+++ recipes-core/systemd/systemd-units/ble.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/bash
 
 NAME=ble
 DESC="Initialization of bluetooth chip"
@@ -17,6 +17,17 @@ start)
 
     # hci0
     hciattach /dev/ttymxc2 texas
+
+    # detection
+    # https://e2e.ti.com/support/wireless-connectivity/bluetooth/f/538/t/542034?distinguish-between-CC2564B-and-CC2564C
+    SYSTEM_STATUS=$(hcitool -i hci0 cmd 0x3F 0x021F)
+    if [[ $SYSTEM_STATUS =~ 1F\ FE\ 00\ 07\ 10 ]]; then
+        echo "<6>CC2564B detected"
+    elif [[ $SYSTEM_STATUS =~ 1F\ FE\ 00\ 0C\ 1A ]]; then
+        echo "<4>CC2564C detected"
+    else
+        echo "<3>CC2564B/C not detected"
+    fi
 ;;
 
 stop)
