09359452e69751062a8fd002c9589a5ef59ab5bf
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2019-04-29 11:35:37 +0200

    Differentiate between full length and size limited PCAN trace file.

--- recipes-core/systemd/systemd-units/candump-save.sh
+++ recipes-core/systemd/systemd-units/candump-save.sh
@@ -18,9 +18,11 @@ name="$1"
 echo "Waiting for 10 seconds to obtain some more candump"
 sleep 10
 
+echo "Commanding multilog to rotate logs and waiting up to 10s..."
 inotifywait -t 10 -e attrib /tmp/candump/current &>/dev/null &
 pkill -SIGALRM multilog &
 wait
+echo "...done"
 
 unset -v latest
 unset -v previousLatest
@@ -30,8 +32,16 @@ for file in "$dir"*; do
     fi
 done
 
-echo "Concatenating $previousLatest $latest and saving as PCAN trace file /mnt/data/candump/$name.trc"
-cat $previousLatest $latest > $dir$name.candump
+# Remark: If this service was called before multilog has rotated logs at least once
+# on its own, previousLatest will be empty and thus cat will only take latest.
+if [ "$name" == "manual" ]; then
+    echo "Concatenating $previousLatest $latest and saving as full length PCAN trace file /mnt/data/candump/$name.trc"
+    cat $previousLatest $latest > $dir$name.candump
+else
+    echo "Concatenating $previousLatest $latest and saving as size limited PCAN trace file /mnt/data/candump/$name.trc"
+    cat $previousLatest $latest | tail -n 5000 > $dir$name.candump
+fi
+
 awk -f /etc/scripts/candump.awk $dir$name.candump > $dir$name.trc
 rm $dir$name.candump
 mkdir -p /mnt/data/candump
