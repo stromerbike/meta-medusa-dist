37b47dd2a1e823dd260936b9d2c05d09ac7204ad
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2022-06-08 17:25:57 +0200

    Use /etc/scripts/hlxxxx-common.sh for interface prepare- and cleanup-handling

--- recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-usbcomp-revert.sh
+++ recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-usbcomp-revert.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+source /etc/scripts/hlxxxx-common.sh
+
+INTERFACE="ttyACM0"
+
 if lsusb -d 1519:0020; then
     echo "HL85xxx in USB mode detected"
 elif lsusb -d 1199:c001; then
     echo "HL78xx in USB mode detected"
-    if [ -e /dev/ttyACM0 ]; then
-        fuser -k /dev/ttyACM0
-    fi
-    if [ -e /var/lock/LCK..ttyACM0 ]; then
-        rm -fv /var/lock/LCK..ttyACM0
-    fi
-    KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0 | grep +KUSBCOMP:)
+    prepareComport
+    KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE" | grep +KUSBCOMP:)
     if [ $? -eq 0 ]; then
+        cleanupComport
         if [[ $KUSBCOMP =~ \+KUSBCOMP:\ 1,1,2,3 ]]; then
             echo "USB mode with desired interface assignments: $KUSBCOMP"
             echo "UART mode activation issued"
-            echo -e "AT+KUSBCOMP=0\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0 >/dev/null
+            prepareComport
+            echo -e "AT+KUSBCOMP=0\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE" >/dev/null
+            cleanupComport
             echo "Resetting module via command"
-            echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0
+            prepareComport
+            echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE"
+            cleanupComport
             exit 0
         else
             echo "USB mode with undesired interface assignments: $KUSBCOMP"
@@ -25,7 +29,8 @@ elif lsusb -d 1199:c001; then
             exit 11
         fi
     else
-        echo "No response received via ttyACM0"
+        cleanupComport
+        echo "No response received via $INTERFACE"
         echo "Run hl78xx-usbcomp.sh first"
         exit 11
     fi
--- recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-usbcomp.sh
+++ recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-usbcomp.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
+source /etc/scripts/hlxxxx-common.sh
+
+INTERFACE="ttymxc7"
+
 if lsusb -d 1519:0020; then
     echo "HL85xxx in USB mode detected"
     exit 0
 elif lsusb -d 1199:c001; then
     echo "HL78xx in USB mode detected"
-    if [ -e /dev/ttyACM0 ]; then
-        fuser -k /dev/ttyACM0
-    fi
-    if [ -e /var/lock/LCK..ttyACM0 ]; then
-        rm -fv /var/lock/LCK..ttyACM0
-    fi
-    KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0 | grep +KUSBCOMP:)
+    INTERFACE="ttyACM0"
+    prepareComport
+    KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE" | grep +KUSBCOMP:)
+    cleanupComport
     if [ $? -eq 0 ]; then
         if [[ $KUSBCOMP =~ \+KUSBCOMP:\ 1,1,2,3 ]]; then
             echo "USB mode with desired interface assignments: $KUSBCOMP"
             exit 0
         else
             echo "USB mode with undesired interface assignments: $KUSBCOMP"
-            echo -e "AT+KUSBCOMP=1,1,2,3\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0 >/dev/null
-            KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0 | grep +KUSBCOMP:)
+            prepareComport
+            echo -e "AT+KUSBCOMP=1,1,2,3\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE" >/dev/null
+            cleanupComport
+            prepareComport
+            KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE" | grep +KUSBCOMP:)
+            cleanupComport
             if [ $? -eq 0 ]; then
                 if [[ $KUSBCOMP =~ \+KUSBCOMP:\ 1,1,2,3 ]]; then
                     echo "Interface assigments adjusted to $KUSBCOMP"
                     echo "Resetting module via command"
-                    echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 microcom -t 1000 /dev/ttyACM0
+                    prepareComport
+                    echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 microcom -t 1000 "/dev/$INTERFACE"
+                    cleanupComport
                     exit 0
                 else
                     echo "Interface assignments could not be adjusted correctly ($KUSBCOMP)"
@@ -36,33 +43,48 @@ elif lsusb -d 1199:c001; then
             fi
         fi
     else
-        echo "No response received via ttyACM0"
+        echo "No response received via $INTERFACE"
         echo "Interface assignments are possibly set to +KUSBCOMP=1,0,0,0"
-        KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7 | grep +KUSBCOMP:)
+        INTERFACE="ttymxc7"
+        prepareComport
+        KUSBCOMP=$(echo -e "AT+KUSBCOMP?\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE" | grep +KUSBCOMP:)
+        cleanupComport
         if [ $? -eq 0 ]; then
             echo "Interface assignments $KUSBCOMP"
-            if echo -e "AT+KUSBCOMP=1,1,2,3\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7 | grep AT+KUSBCOMP=1,1,2,3; then
+            prepareComport
+            if echo -e "AT+KUSBCOMP=1,1,2,3\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE" | grep AT+KUSBCOMP=1,1,2,3; then
+                cleanupComport
                 echo "USB mode activated. Resetting module via command."
-                echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7
+                prepareComport
+                echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE"
+                cleanupComport
                 exit 0
             else
+                cleanupComport
                 echo "USB mode could not be activated"
                 exit 11
             fi
         else
-            echo "Interface assignments could not be read via ttymxc7"
+            echo "Interface assignments could not be read via $INTERFACE"
             exit 11
         fi
     fi
 else
-    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7 | grep ^HL78)
+    prepareComport
+    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE" | grep ^HL78)
+    cleanupComport
     if [[ $CGMR =~ HL78 ]]; then
         echo "HL78xx (${CGMR//$'\r'/}) in UART mode detected. Issuing AT+KUSBCOMP=1,1,2,3."
-        if echo -e "AT+KUSBCOMP=1,1,2,3\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7 | grep AT+KUSBCOMP=1,1,2,3; then
+        prepareComport
+        if echo -e "AT+KUSBCOMP=1,1,2,3\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE" | grep AT+KUSBCOMP=1,1,2,3; then
+            cleanupComport
             echo "USB mode activated. Resetting module via command."
-            echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7
+            prepareComport
+            echo -e "AT+CFUN=1,1\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE"
+            cleanupComport
             exit 0
         else
+            cleanupComport
             echo "USB mode could not be activated"
             exit 11
         fi
