270c9b5ae6809f148b4231377007a31bc5473e03
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2022-06-10 20:49:44 +0200

    Reworked hl78xx-update.sh for usage in the field. While making use of hlxxxx-common.sh for interface handling.

--- recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-update-revert.sh
+++ recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-update-revert.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+source /etc/scripts/hlxxxx-common.sh
+
+INTERFACE="ttymxc7"
+
 if lsusb -d 1519:0020; then
     echo "HL85xxx in USB mode detected"
     exit 0
 elif lsusb -d 1199:c001; then
     echo "HL78xx in USB mode detected"
+    # Remark:
+    # The following command seems to work when the module is in USB mode:
+    # sft -d -p /dev/ttyGSM1 -g /dev/ttyGSMBL *
+    # Since unstable transmissions were observed which led to modules only
+    # recoverable via debug UART, sft in USB mode is not supported by this script.
     echo "Run hl78xx-usbcomp-revert.sh first"
     exit 11
 else
-    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7 | grep ^HL78)
+    prepareComport
+    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 "/dev/$INTERFACE" | grep ^HL78)
+    cleanupComport
     if [[ $CGMR =~ ^HL78([0-9]+) ]]; then
         MODULE_VARIANT="${BASH_REMATCH[1]}"
         echo "HL78$MODULE_VARIANT detected"
@@ -16,12 +27,11 @@ else
             if sha256sum -c SHA256SUMS; then
                 echo "Checksums valid"
                 cat /lib/firmware/sierra-wireless/HL78$MODULE_VARIANT.*/package_version | grep HL78$MODULE_VARIANT
-                # Remark: Currently unclear why restarting the gsm service is necessary.
-                echo "Restarting gsm service and waiting 10 seconds..."
-                systemctl stop gsm
+                # Remark: Currently unclear why restarting the gsm service is necessary.    
+                #         Wait some reconds afterwards to keep sft from trying other baudrates already.
+                echo "Restarting gsm service..."
+                systemctl restart gsm
                 sleep 5
-                systemctl start gsm
-                sleep 10
                 echo "...done"
                 echo ""
                 echo "##############################"
@@ -29,11 +39,16 @@ else
                 echo "##############################"
                 echo "ABORTING THIS PROCEDURE (CTRL+C, POWER-CUT) WILL BRICK THE MODULE..."
                 echo ""
-                if sft -d -b 115200 -p /dev/ttymxc7 *; then
+                # Remark: Recovery of a bricked module is possible by connecting the debug UART
+                #         via their testpoints and running sft on a PC via e.g. ttyUSBx.
+                prepareComport
+                if sft -d -b 115200 -p "/dev/$INTERFACE" *; then
+                    cleanupComport
                     echo "Installation suceeded"
                     echo "...it is now safe to continue"
                     exit 0
                 else
+                    cleanupComport
                     echo "Installation failed"
                     echo "...it is now safe to continue"
                     exit 11
--- recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-update.sh
+++ recipes-medusa/medusa-scripts/medusa-scripts/hl78xx-update.sh
@@ -1,52 +1,62 @@
 #!/bin/bash
 
-SEND_COMMAND=""
-SEND_INTERFACE=""
+source /etc/scripts/hlxxxx-common.sh
+
+CGMR=""
+COMMAND=""
+INTERFACE="ttyACM1" # The update via XMODEM, resp. AT+WDSD= only works on the "AT/PPP data port".
 
 if lsusb -d 1519:0020; then
     echo "HL85xxx in USB mode detected"
     exit 0
 elif lsusb -d 1199:c001; then
+    # When wvdial has previously been active on the interface to be used,
+    # the first command might fail, because data mode is still active.
     COUNTER=0
     while [ $COUNTER -lt 5 ];
     do
         let COUNTER=COUNTER+1
-        if [ -e /dev/ttyACM1 ]; then
-            fuser -k /dev/ttyACM1
-        fi
-        if [ -e /var/lock/LCK..ttyACM1 ]; then
-            rm -fv /var/lock/LCK..ttyACM1
-        fi
-        CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 microcom -t 1000 "/dev/ttyACM1" | grep ^HL78)
+        prepareComport
+        CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 3 microcom -t 2000 "/dev/$INTERFACE" | grep ^HL78)
+        cleanupComport
         if [ ! -z "$CGMR" ]; then
             echo "HL78xx in USB mode detected: $CGMR"
-            SEND_COMMAND="microcom -t 1000"
-            SEND_INTERFACE="ttyACM1"
+            COMMAND="timeout -s KILL 3 microcom -t 2000"
             break
         fi
     done
-    if [ -z "$SEND_COMMAND" ] && [ -z "$SEND_INTERFACE" ]; then
+    if [ -z "$CGMR" ]; then
         echo "HL78xx in USB mode not responsive"
         exit 11
     fi
 else
-    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 picocom -qr -b 115200 -f h -x 1000 /dev/ttymxc7 | grep ^HL78)
+    INTERFACE="ttymxc7"
+    prepareComport
+    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 3 picocom -qr -b 115200 -f h -x 2000 "/dev/$INTERFACE" | grep ^HL78)
+    cleanupComport
     if [ ! -z "$CGMR" ]; then
         echo "HL78xx in UART mode detected: $CGMR"
-        SEND_COMMAND="picocom -qr -b 115200 -f h -x 1000"
-        SEND_INTERFACE="ttymxc7"
+        COMMAND="timeout -s KILL 3 picocom -qr -b 115200 -f h -x 2000"
     else
         echo "No answer via UART received"
         exit 11
     fi
 fi
 
-if [ ! -z "$SEND_COMMAND" ] && [ ! -z "$SEND_INTERFACE" ]; then
-    CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 $SEND_COMMAND "/dev/$SEND_INTERFACE" | grep ^HL78)
-    if [[ $CGMR =~ ^HL78([0-9]+).([0-9]+.[0-9]+.[0-9]+.[0.9]+) ]]; then
+echo "COMMAND: $COMMAND"
+echo "INTERFACE: $INTERFACE"
+
+if [ ! -z "$CGMR" ]; then
+    prepareComport
+    CGMR=$(echo -e "AT+CGMR\r" | $COMMAND "/dev/$INTERFACE" | grep ^HL78)
+    cleanupComport
+    if [[ $CGMR =~ ^HL78([0-9]+).(([0-9]+).([0-9]+).([0-9]+).([0-9]+)) ]]; then
         MODULE_VARIANT="${BASH_REMATCH[1]}"
         CURRENT_REVISION="${BASH_REMATCH[2]}"
-        echo "Current HL78$MODULE_VARIANT revision: $CURRENT_REVISION"
+        # Some firmware versions have more than one digit per position in their version.
+        # For working nummerical comparisons, "0"'s are prepended for each position.
+        CURRENT_REVISION_ZEROED="$(printf "%03d%03d%03d%03d" ${BASH_REMATCH[3]} ${BASH_REMATCH[4]} ${BASH_REMATCH[5]} ${BASH_REMATCH[6]})"
+        echo "Current HL78$MODULE_VARIANT revision: $CURRENT_REVISION ($CURRENT_REVISION_ZEROED)"
         if cd /lib/firmware/sierra-wireless/ && sha256sum -c SHA256SUMS; then
             echo "Checksums valid"
             UPDATE_FILE="$(stat -c %n HL78${MODULE_VARIANT}_${CURRENT_REVISION}_to_*)"
@@ -54,45 +64,82 @@ if [ ! -z "$SEND_COMMAND" ] && [ ! -z "$SEND_INTERFACE" ]; then
                 echo "Applicable update file: $UPDATE_FILE"
                 UPDATE_FILE_SIZE="$(stat -c %s $UPDATE_FILE)"
                 echo "Update file size: $UPDATE_FILE_SIZE bytes"
-                if echo -e "AT+WDSI=4470\r" | timeout -s KILL 2 $SEND_COMMAND "/dev/$SEND_INTERFACE" | grep OK; then
+                prepareComport
+                if echo -e "AT+WDSI=4470\r" | $COMMAND "/dev/$INTERFACE" | grep OK; then
+                    cleanupComport
                     echo "Activated all indications"
                 else
+                    cleanupComport
                     echo "Could not activate all indications"
                     exit 11
                 fi
-                if echo -e "ATE1\r" | timeout -s KILL 2 $SEND_COMMAND "/dev/$SEND_INTERFACE" | grep OK; then
-                    echo "Activated echo"
+                prepareComport
+                if echo -e "AT+WDSD=$UPDATE_FILE_SIZE\r" | $COMMAND "/dev/$INTERFACE" | grep -m1 -a $'\x15'; then
+                    cleanupComport
+                    echo "Sent file size and received <NACK> character(s)"
                 else
-                    echo "Could not activate echo"
+                    cleanupComport
+                    echo "Could not send file size or did not receive <NACK> character(s)"
                     exit 11
                 fi
-                if echo -e "AT+WDSD=$UPDATE_FILE_SIZE\r" | timeout -s KILL 2 $SEND_COMMAND "/dev/$SEND_INTERFACE" | grep AT+WDSD=$UPDATE_FILE_SIZE; then
-                    echo "Sent file size"
-                else
-                    echo "Could not send file size"
-                    exit 11
-                fi
-                if sx -vv --1k --xmodem $UPDATE_FILE < "/dev/$SEND_INTERFACE" > "/dev/$SEND_INTERFACE"; then
+                prepareComport
+                # Remark: X-MODEM transfers with 1024 byte blocks may fail for given delta image sizes (EURY-4127, fixed in 4.6.9.4).
+                #         The following HL7802 firmwares have been tested in this regard
+                #         (the calculation method EURY-4127 does not always seem valid):
+                #         - HL7802_4.3.9.0_to_4.4.14.0_allBin_nbIOT11_sig11.ua: --1k works
+                #         - HL7802_4.3.9.0_to_4.6.9.4_allBin_nbIOT11_sig11.ua:  --1k does not work (ERROR)
+                #         - HL7802_4.4.14.0_to_4.6.9.4_allBin_nbIOT11_sig11.ua: --1k does not work (CME ERROR 3)
+                #         - HL7802_4.4.14.0_to_4.5.4.0_allBin_nbIOT11_sig11.ua: --1k works
+                #         - HL7802_4.5.4.0_to_4.6.9.4_allBin_nbIOT11_sig11.ua:  --1k works
+                #
+                #         To make use of the considerably faster 1024 byte block transfer, an update to 4.6.9.4+
+                #         over intermediate versions (4.4.14.0 -> 4.5.4.0 -> 4.6.9.4) should be strongly favored.
+                if sx -vv --1k --xmodem $UPDATE_FILE < "/dev/$INTERFACE" > "/dev/$INTERFACE"; then
+                    cleanupComport
                     echo "Sent file"
                 else
+                    cleanupComport
                     echo "Could not send file"
                     exit 11
                 fi
                 sleep 5
-                if echo -e "AT+WDSR=4\r" | timeout -s KILL 2 $SEND_COMMAND "/dev/$SEND_INTERFACE" | grep "+WDSI: 14"; then
+                prepareComport
+                if echo -e "AT+WDSR=4\r" | $COMMAND "/dev/$INTERFACE" | grep "+WDSI: 14"; then
+                    cleanupComport
                     echo "Accepted the installation and update will be launched"
                     echo "Waiting up to 300s for the installation to complete"
+                    # Upon apperance of the of the ttyACM1 (symlinked to ttyGSM1) interface,
+                    # it will be used by the wvdial service and should not be used in this script.
+                    # Therefore use the cgmr value read out by the gsm-module service.
+                    if [[ $INTERFACE == ttyACM* ]]; then
+                        if [ -f /tmp/cgmr ]; then
+                            rm -fv /tmp/cgmr
+                        fi
+                    fi
                     COUNTER=0
                     while [ $COUNTER -lt 30 ];
                     do
                         COUNTER=$((COUNTER+1))
                         sleep 8
-                        CGMR=$(echo -e "AT+CGMR\r" | timeout -s KILL 2 $SEND_COMMAND "/dev/$SEND_INTERFACE" | grep ^HL78)
-                        if [[ $CGMR =~ ^HL78([0-9]+).([0-9]+.[0-9]+.[0-9]+.[0.9]+) ]]; then
+                        if [[ $INTERFACE == ttyACM* ]]; then
+                            CGMR="$(cat /tmp/cgmr 2> /dev/null)"
+                            sleep 2
+                        else
+                            prepareComport
+                            CGMR=$(echo -e "AT+CGMR\r" | $COMMAND "/dev/$INTERFACE" | grep ^HL78)
+                            cleanupComport
+                        fi
+                        if [[ $CGMR =~ ^HL78([0-9]+).([0-9]+.[0-9]+.[0-9]+.[0-9]+) ]]; then
                             NEW_REVISION="${BASH_REMATCH[2]}"
                             if [[ "$NEW_REVISION" != "$CURRENT_REVISION" ]]; then
                                 echo "Revision has changed to $NEW_REVISION"
-                                exit 0
+                                /etc/scripts/hl78xx-update-check.sh $CGMR
+                                if [ $? -eq 1 ]; then
+                                    # Raise a "Package not installed" in case there is still an applicable update file.
+                                    exit 65
+                                else
+                                    exit 0
+                                fi
                             else
                                 echo "Revision has not changed from $CURRENT_REVISION"
                                 exit 11
@@ -104,12 +151,13 @@ if [ ! -z "$SEND_COMMAND" ] && [ ! -z "$SEND_INTERFACE" ]; then
                     echo "Could not read back revision"
                     exit 11
                 else
+                    cleanupComport
                     echo "Could not accept the installation and update will not be launched"
                     exit 11
                 fi
             else
                 echo "No applicable update file found"
-                if [[ "$CURRENT_REVISION" > "4.3.9.0" ]]; then
+                if [ $((10#$CURRENT_REVISION_ZEROED)) -ge 4004014000 ]; then # 4.4.14.0 and newer
                     echo "Current revision is new enough"
                     exit 0
                 else
