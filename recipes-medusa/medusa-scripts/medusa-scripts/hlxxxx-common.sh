d4a4fed9fbddf234af89353cc1fec493b37727ba
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2022-11-11 17:10:49 +0100

    Adopted for caller scripts using "set -e".

--- recipes-medusa/medusa-scripts/medusa-scripts/hlxxxx-common.sh
+++ recipes-medusa/medusa-scripts/medusa-scripts/hlxxxx-common.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 ###
-# Realizes a forceful shutdown (KILL) of the application accessing the given interface.
-# Also removes any potentially existing interface lock.
+# Brief:
+#   Realizes a forceful shutdown (KILL) of the application accessing the given interface.
+#   Also removes any potentially existing interface lock.
+# Details:
+#   Because in most situations no forceful shutdown (KILL) resp. no preparation is required,
+#   "|| true" is appended to the fuser call so that a caller script using "set -e" is not terminated.
 # Globals:
 #   INTERFACE (readonly)
 # Arguments: -
@@ -12,7 +16,7 @@
 ###
 function prepareComport() {
     if [ -e "/dev/$INTERFACE" ]; then
-        fuser -k "/dev/$INTERFACE"
+        fuser -k "/dev/$INTERFACE" || true
     fi
     if [ -e "/var/lock/LCK..$INTERFACE" ]; then
         rm -fv "/var/lock/LCK..$INTERFACE"
@@ -20,7 +24,11 @@ function prepareComport() {
 }
 
 ###
-# Realizes a graceful shutdown (TERM) of the application accessing the given interface.
+# Brief:
+#   Realizes a graceful shutdown (TERM) of the application accessing the given interface.
+# Details:
+#   Because in many situations no graceful shutdown (TERM) resp. no cleanup is required,
+#   "|| true" is appended to the fuser call so that a caller script using "set -e" is not terminated.
 # Globals:
 #   INTERFACE (readonly)
 # Arguments: -
@@ -30,6 +38,6 @@ function prepareComport() {
 ###
 function cleanupComport() {
     if [ -e "/dev/$INTERFACE" ]; then
-        fuser -s -k -TERM "/dev/$INTERFACE"
+        fuser -s -k -TERM "/dev/$INTERFACE" || true
     fi
 }
