610dd042c24955a99249f57c2a2998bbe9a8f0af
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2023-02-13 11:53:24 +0100

    Actively drive CAN_SILENT to low for CAN transceivers with standby mode.

--- recipes-core/systemd/systemd-units/can0.sh
+++ recipes-core/systemd/systemd-units/can0.sh
@@ -5,6 +5,13 @@ DESC="Initialization of can0 interface"
 
 case $1 in
 start)
+    # CAN_SILENT
+    # GPIO5 IO09 => (5 - 1) * 32 + 9 = 137
+    # Remark: Pin compatible CAN transceivers to the TCAN1051 such as the TCAN1042
+    # or MAX13054 with a standby feature need their STBY input driven low to work.
+    test -e /sys/class/gpio/gpio137 || echo "137" > /sys/class/gpio/export
+    echo "low" > /sys/class/gpio/gpio137/direction
+
     while [ ! -d "/sys/devices/soc0/soc/2000000.aips-bus/2094000.flexcan/net/can0" ];
     do
         sleep 1
