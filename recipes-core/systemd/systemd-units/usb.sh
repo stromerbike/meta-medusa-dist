ed757b5c105285c7a8b4f324fb64f83d97408052
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2024-05-06 14:25:43 +0200

    Use one liner to initialize and set state of GPIOs used as outputs.
    For some services like ble-attach and gsm the change has already been made.
    Since with the kirkstone update, a broader testing coverage is required,
    its now or never.

--- recipes-core/systemd/systemd-units/usb.sh
+++ recipes-core/systemd/systemd-units/usb.sh
@@ -7,18 +7,15 @@ case $1 in
 start)
     # FORCE_OTG1_ID
     test -e /sys/class/gpio/gpio23 || echo "23" > /sys/class/gpio/export
-    echo "out" > /sys/class/gpio/gpio23/direction
-    echo "1" > /sys/class/gpio/gpio23/value
+    echo "high" > /sys/class/gpio/gpio23/direction
 
     # ILIM_SEL = ILIM_LO (1A)
     test -e /sys/class/gpio/gpio504 || echo "504" > /sys/class/gpio/export
-    echo "out" > /sys/class/gpio/gpio504/direction
-    echo "0" > /sys/class/gpio/gpio504/value
+    echo "low" > /sys/class/gpio/gpio504/direction
 
     # CTL2
     test -e /sys/class/gpio/gpio506 || echo "506" > /sys/class/gpio/export
-    echo "out" > /sys/class/gpio/gpio506/direction
-    echo "1" > /sys/class/gpio/gpio506/value
+    echo "high" > /sys/class/gpio/gpio506/direction
 ;;
 
 stop)
