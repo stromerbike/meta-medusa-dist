c6515f37c5f3c4964a879c76a675016de88c5dfa
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2024-06-28 12:23:09 +0200

    Wrapped btmgmt by script to fake a tty and fix shutdown hanging.
    See https://stackoverflow.com/questions/66532619/redirecting-output-of-bluez-btmgmt-to-file-from-systemd-service

--- recipes-core/systemd/systemd-units/ble-attach.sh
+++ recipes-core/systemd/systemd-units/ble-attach.sh
@@ -21,8 +21,8 @@ start)
 ;;
 
 stop)
-    # Turn off the Bluetooth radio using btmgmt
-    btmgmt power off
+    # Turn off the Bluetooth radio using btmgmt ('echo -e "power off\nquit\n" | bluetoothctl' could also be used)
+    script -qc "btmgmt power off"
 
     # Kill all existing hciattach processes (will also take down hci interface)
     killall hciattach
@@ -32,8 +32,8 @@ stop)
 ;;
 
 reload)
-    # Turn off the Bluetooth radio using btmgmt
-    btmgmt power off
+    # Turn off the Bluetooth radio using btmgmt ()
+    script -qc "btmgmt power off"
 
     # Kill all existing hciattach processes (will also take down hci interface)
     killall hciattach
