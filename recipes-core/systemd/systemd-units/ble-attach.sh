226e7a21691411ab8256cfbb700edea79195f950
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2024-10-02 15:44:36 +0200

    Improved hciattach retrial handling. - Check presence via "hciconfig hci0"'s return value rather than hciattach one. - Issue a reload in between hciattach calls.
    Remark:
    This handling has not (yet) been tested.
    But the reasoning for adding it, is that some kind of handling
    is better than not having a working Bluetooth interface.

--- recipes-core/systemd/systemd-units/ble-attach.sh
+++ recipes-core/systemd/systemd-units/ble-attach.sh
@@ -3,6 +3,27 @@
 NAME=ble
 DESC="Initialization of bluetooth chip"
 
+reload() {
+    # Turn off the Bluetooth radio using btmgmt ('echo -e "power off\nquit\n" | bluetoothctl' could also be used)
+    script -qc "btmgmt power off" /dev/null
+
+    # Kill all existing hciattach processes (will also take down hci interface)
+    killall hciattach
+
+    # BLE_nSHUTD (put device in low power mode and do internal reset)
+    echo "0" > /sys/class/gpio/gpio120/value
+    # Wait (minimum time for nSHUT_DOWN low to reset the device: 5ms)
+    sleep 0.1
+    # BLE_nSHUTD (bring device out of low power mode)
+    echo "1" > /sys/class/gpio/gpio120/value
+
+    # Attach the HCI device using hciattach
+    hciattach /dev/ttymxc2 texas
+
+    # Power on the Bluetooth radio using btmgmt ('echo -e "power on\nquit\n" | bluetoothctl' could also be used)
+    script -qc "btmgmt power on" /dev/null
+}
+
 case $1 in
 start)
     # BLE_nON
@@ -15,16 +36,20 @@ start)
 
     # hci0
     # Despite 0001-tools-hciattach-Increase-timeout-for-TI-specific-ini.patch in place
-    # (with the timeout made even bigger: 1000ms) cases have still been observed where
-    # hciattach fails. Retrials shall help to alleviate the issue.
+    # cases have still been observed where hciattach fails.
+    # A presence check of hci0 and retries shall help to alleviate the issue.
+    hciattach /dev/ttymxc2 texas
     COUNTER=0
     while [ $COUNTER -lt 5 ];
     do
         COUNTER=$((COUNTER+1))
-        if hciattach /dev/ttymxc2 texas; then
+        if hciconfig hci0; then
             break
+        else
+            echo "<3>hci0 not present, retrying..."
+            reload
         fi
-        sleep 3
+        sleep 5
     done
 
     # Bluetooth daemon "/usr/libexec/bluetooth/bluetoothd" needs to be started before the hci0 interface is up !!! (ScUr: Not sure if still applicable)
@@ -43,24 +68,7 @@ stop)
 ;;
 
 reload)
-    # Turn off the Bluetooth radio using btmgmt ('echo -e "power off\nquit\n" | bluetoothctl' could also be used)
-    script -qc "btmgmt power off" /dev/null
-
-    # Kill all existing hciattach processes (will also take down hci interface)
-    killall hciattach
-
-    # BLE_nSHUTD (put device in low power mode and do internal reset)
-    echo "0" > /sys/class/gpio/gpio120/value
-    # Wait (minimum time for nSHUT_DOWN low to reset the device: 5ms)
-    sleep 0.1
-    # BLE_nSHUTD (bring device out of low power mode)
-    echo "1" > /sys/class/gpio/gpio120/value
-
-    # Attach the HCI device using hciattach
-    hciattach /dev/ttymxc2 texas
-
-    # Power on the Bluetooth radio using btmgmt
-    btmgmt power on
+    reload
 ;;
 
 *)
