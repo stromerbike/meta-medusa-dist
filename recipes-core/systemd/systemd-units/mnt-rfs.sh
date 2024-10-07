fb13c5e0a5614d774806cf4509086ee510a1fced
tristan.ramseyer <tristan.ramseyer@stromerbike.com>
2024-10-07 11:31:37 +0200

    Added support for reload in mnt-rfs.service.

--- recipes-core/systemd/systemd-units/mnt-rfs.sh
+++ recipes-core/systemd/systemd-units/mnt-rfs.sh
@@ -3,15 +3,28 @@
 NAME=mnt-rfs
 DESC="Mount the inactive rfs partition"
 
+inactive_partition=""
+if df / | grep 'ubi0:part0'; then
+    inactive_partition="/dev/ubi0_1"
+elif df / | grep 'ubi0:part1'; then
+    inactive_partition="/dev/ubi0_0"
+fi
+
+reload() {
+    umount -v -f /mnt/rfs_inactive || true
+    echo "Creating an empty filesystem on $inactive_partition..."
+    if mkfs.ubifs -v -y "$inactive_partition"; then
+        echo "Re-mounting inactive rfs partition $inactive_partition..."
+        if mount -t ubifs "$inactive_partition" /mnt/rfs_inactive; then
+            echo "...done"
+            exit 0
+        fi
+    fi
+    echo "<3>...ERROR"
+}
+
 case $1 in
 start)
-    inactive_partition=""
-    if df / | grep 'ubi0:part0'; then
-        inactive_partition="/dev/ubi0_1"
-    elif df / | grep 'ubi0:part1'; then
-        inactive_partition="/dev/ubi0_0"
-    fi
-
     if [ -n "$inactive_partition" ]; then
         echo "Mounting inactive rfs partition $inactive_partition..."
         if mount -t ubifs "$inactive_partition" /mnt/rfs_inactive; then
@@ -26,16 +39,7 @@ start)
         fi
         echo "<3>...ERROR"
 
-        umount -v -f /mnt/rfs_inactive || true
-        echo "Creating an empty filesystem on $inactive_partition..."
-        if mkfs.ubifs -v -y "$inactive_partition"; then
-            echo "Re-mounting inactive rfs partition $inactive_partition..."
-            if mount -t ubifs "$inactive_partition" /mnt/rfs_inactive; then
-                echo "...done"
-                exit 0
-            fi
-        fi
-        echo "<3>...ERROR"
+        reload
     else
         echo "Active partition could not be determined"
     fi
@@ -44,6 +48,15 @@ start)
 stop)
     echo "Unmounting inactive rfs partition..."
     umount /mnt/rfs_inactive
+;;
+
+reload)
+    if [ -n "$inactive_partition" ]; then
+        fuser -k -m /mnt/rfs_inactive
+        reload
+    else
+        echo "Active partition could not be determined"
+    fi
 ;;
 
 *)
